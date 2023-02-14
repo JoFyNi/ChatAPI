fc74227721bc4991cdcc28321204e2ed60e850a2
Josh <jnievelstein@yahoo.com>
2023-02-14 16:08:47 +0100

    V2 Server.java Client.java

--- simpleChat/src/main/java/v2/Client.java
+++ simpleChat/src/main/java/v2/Client.java
@@ -5,13 +5,20 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 import java.util.Scanner;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Client {
     public static void main(String[] args) throws IOException {
+        ClientTest();
+    }
+
+    public static void ClientTest() throws IOException {
         Socket socket = new Socket("localhost", 8888);
         System.out.println("Connected to server");
         DataInputStream inputStream = new DataInputStream(socket.getInputStream());
         DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
+        getMessage(inputStream);
         Scanner scanner = new Scanner(System.in);
         String message = "";
         while (!message.equals("exit chat")) {
@@ -19,13 +26,31 @@ public class Client {
             message = scanner.nextLine();
             outputStream.writeUTF(message);
             outputStream.flush();
-            message = inputStream.readUTF();
-            System.out.println("Server: " + message);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
-
         inputStream.close();
         outputStream.close();
         socket.close();
         scanner.close();
+    }
+
+    public static void getMessage(DataInputStream inputStream) throws IOException {
+        java.util.Timer timer = new Timer();
+        TimerTask task = new TimerTask() {
+            public void run() {
+                String message = "";
+                try {
+                    message = inputStream.readUTF();
+                    System.out.println("Server: " + message);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        };
+        timer.schedule(task, 0, 100);
     }
 }
--- simpleChat/src/main/java/v2/Server.java
+++ simpleChat/src/main/java/v2/Server.java
@@ -5,13 +5,15 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Server {
     private static List<String> logs = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
+        serverTest();
+    }
+    public static void serverTest() throws IOException {
         ServerSocket serverSocket = new ServerSocket(8888);
         System.out.println("Server started on port 8888");
         while (true) {
@@ -19,17 +21,25 @@ public class Server {
             System.out.println("Client connected");
             DataInputStream inputStream = new DataInputStream(socket.getInputStream());
             DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
+            getMessage(inputStream);
             try {
+                Scanner scanner = new Scanner(System.in);
                 String message = "";
                 while (!message.equals("exit chat")) {
-                    message = inputStream.readUTF();
-                    System.out.println("Client: " + message);
-                    logs.add("Client: " + message);
+
+                    System.out.print("You: ");
+                    message = scanner.nextLine();
                     outputStream.writeUTF(message);
                     outputStream.flush();
-                    message = inputStream.readUTF();
-                    System.out.println("Server: " + message);
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                 }
+                //message = inputStream.readUTF();
+                //System.out.println("Client: " + message);
+                //logs.add("Client: " + message);
             } catch (IOException e) {
                 System.out.println("Client disconnected");
             } finally {
@@ -38,6 +48,22 @@ public class Server {
                 socket.close();
             }
         }
+    }
+    public static void getMessage(DataInputStream inputStream) throws IOException {
+        java.util.Timer timer = new Timer();
+        TimerTask task = new TimerTask() {
+            public void run() {
+                String message = "";
+                try {
+                    message = inputStream.readUTF();
+                    System.out.println("Client: " + message);
+                    logs.add("Client: " + message);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        };
+        timer.schedule(task, 0, 100);
     }
 
     public static List<String> getLogs() {
